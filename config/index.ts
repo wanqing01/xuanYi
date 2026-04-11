8fd2031b165fd35432aa13a014edaba515b03b59
wanqing <314115488@qq.com>
2026-04-11 22:01:45 +0800

    fix: init

---
+++ config/index.ts
@@ -0,0 +1,84 @@
+import { defineConfig, type UserConfigExport } from '@tarojs/cli'
+
+export default defineConfig<'webpack5'>({
+  projectName: 'xuanYi',
+  date: '2026-4-9',
+  designWidth: 750,
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    375: 2,
+    828: 1.81 / 2
+  },
+  sourceRoot: 'src',
+  outputRoot: 'dist',
+  plugins: [],
+  defineConstants: {},
+  copy: {
+    patterns: [],
+    options: {}
+  },
+  framework: 'react',
+  compiler: 'webpack5',
+  cache: {
+    enable: false
+  },
+  mini: {
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {}
+      },
+      cssModules: {
+        enable: false,
+        config: {
+          namingPattern: 'module',
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    }
+  },
+  h5: {
+    publicPath: '/',
+    staticDirectory: 'static',
+    indexPath: 'index.html',
+    devServer: {
+      client: {
+        overlay: false
+      }
+    },
+    output: {
+      filename: 'js/[name].[hash:8].js',
+      chunkFilename: 'js/[name].[chunkhash:8].js'
+    },
+    miniCssExtractPluginOption: {
+      ignoreOrder: true,
+      filename: 'css/[name].[hash].css',
+      chunkFilename: 'css/[name].[chunkhash].css'
+    },
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {}
+      },
+      cssModules: {
+        enable: false,
+        config: {
+          namingPattern: 'module',
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    },
+    webpackChain(chain) {
+      chain.set('ignoreWarnings', [/webpackExports/])
+    }
+  },
+  rn: {
+    appName: 'xuanYi',
+    postcss: {
+      cssModules: {
+        enable: false
+      }
+    }
+  }
+} as UserConfigExport<'webpack5'>)
